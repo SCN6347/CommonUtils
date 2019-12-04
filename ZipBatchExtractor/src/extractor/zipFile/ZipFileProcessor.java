2ac5c82c5499dc046de91d5ad032bc0f79106fa5
SCN6347 <scn637@gmail.com>
2019-12-05 00:42:23 +0530

    Process Zip Folder Completed

--- ZipBatchExtractor/src/extractor/zipFile/ZipFileProcessor.java
+++ ZipBatchExtractor/src/extractor/zipFile/ZipFileProcessor.java
@@ -1,10 +1,17 @@
 package extractor.zipFile;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Enumeration;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipException;
+import java.util.zip.ZipFile;
 
 public class ZipFileProcessor
 {
@@ -14,8 +21,11 @@ public class ZipFileProcessor
 		new File(extractionFolder).mkdirs();
 		try
 		{
+			String formatter = extractionFolder.concat("%s");
 			Files.list(Paths.get(zipFileFolder)).map(Path::toFile).filter(fileObj -> fileObj.getName().endsWith(".zip")).forEach(fileObj-> {
-				
+			
+				String destinationFolderPath = String.format(formatter, fileObj.getName().replace(".zip", ""));
+				performZipFileExtraction(destinationFolderPath, fileObj);
 			});
 		}
 		catch (IOException e)
@@ -29,6 +39,43 @@ public class ZipFileProcessor
 		return new File(zipFileFolder).exists();
 	}
 
+	
+	private void performZipFileExtraction(String destinationFolderPath, File fileObj)
+	{
+		new File(destinationFolderPath).mkdirs();
+		try
+		{
+			ZipFile file = new ZipFile(fileObj);
+			Enumeration<? extends ZipEntry> entries = file.entries();
+			while (entries.hasMoreElements())
+			{
+				ZipEntry entry = entries.nextElement();
+				if (!entry.isDirectory())
+				{
+					InputStream inStream = file.getInputStream(entry);
+					String uncompressedFileName = destinationFolderPath.concat(File.separator).concat(entry.getName());
+					FileOutputStream fileOutput = new FileOutputStream(uncompressedFileName );
+					inStream.transferTo(fileOutput);
+					inStream.close();
+					fileOutput.close();
+				}
+			}
+			file.close();
+		}
+		catch (FileNotFoundException e)
+		{
+			e.printStackTrace();
+		}
+		catch (ZipException e)
+		{
+			e.printStackTrace();
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
 	public void extractZipFile(String zipFile, String extractionFolder)
 	{
 		
