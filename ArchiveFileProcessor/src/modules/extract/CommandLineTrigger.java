ae982f692d841053e1c248031fb9250f8b3c3573
SCN6347 <scn637@gmail.com>
2019-12-05 16:18:08 +0530

    Changed the trigger to Factory Based

--- ArchiveFileProcessor/src/modules/extract/CommandLineTrigger.java
+++ ArchiveFileProcessor/src/modules/extract/CommandLineTrigger.java
@@ -5,7 +5,10 @@ public class CommandLineTrigger
 
 	public static void main(String[] args)
 	{
-
+		String zipFileFolder = null;
+		String extractionFolder = null;
+		FileProcessor fileProcessor = new FileProcessorFactory().getArchiveFileProcessor(ArchiveType.ZIP);
+		fileProcessor.processFileFolder(zipFileFolder, extractionFolder);
 	}
 
 }
